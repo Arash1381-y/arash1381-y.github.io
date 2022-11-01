8bc6d8e07cc177842a9872e5a71a52f2d5914a17
Arash <arashyadegari0402@gmail.com>
2022-11-01 16:49:26 +0330

    add introduction

---
+++ src/constants/pallate.js
@@ -0,0 +1,12 @@
+const primary = '#1C3879';
+const secondary = '#256D85';
+const tertiary = '#CDFCF6';
+
+const background = '#FAF7F0';
+
+export default {
+    primary,
+    secondary,
+    tertiary,
+    background
+}
--- src/sections/Introduction.jsx
+++ src/sections/Introduction.jsx
@@ -1,47 +1,96 @@
 import React from 'react';
-import {Container, Divider, Grid, Typography} from "@mui/material";
+import {Container, Divider, Grid, styled, Typography} from "@mui/material";
 
-import img from '../assests/images/img.png';
+import me from '../assests/images/me.jpg';
+import color from "../constants/pallate";
 
-//create a component with title and image on the right side
+const headerGridStyle = {
+    placeContent: 'center',
+    alignItems: 'center',
+    display: 'grid',
+}
+
+const headerTextStyle = {
+    color: color.primary,
+    fontWeight: 'bold',
+    marginBottom: '1 rem',
+    fontFamily: 'Rubik',
+}
+
+const Paragraph = styled(Typography)({
+    fontFamily: 'Rubik',
+    fontSize: '1.2rem',
+    marginTop: '0.5rem',
+    marginBottom: '0.8rem',
+});
+
+const headerContainerStyle = {
+    marginTop: '2.5rem',
+    marginBottom: '2.5rem',
+};
+
+const Highlight = styled(Typography)({
+    color: color.secondary,
+    fontWeight: 'bold',
+    display: 'inline',
+    fontStyle: 'italic',
+    fontSize: '1.2rem',
+});
+
+
 export default function Introduction() {
     return (
-        <Container maxWidth={'md'} sx={{marginTop: '2.5rem'}}>
-            <Grid container spacing={7}>
-                <Grid display={'grid'} sx={{placeContent: 'center'}} alignItems={'center'} alignContent={'center'} item
-                      xs={12} sm={7}>
-                    <Typography fontWeight={'bold'} align={'center'} variant={'h4'} sx={{marginBottom: '1rem'}}>
-                        Hi, this is me, Im a happy donkey
-                    </Typography>
-                    <Typography   align={'center'} variant={'caption'} color={'textSecondary'}>
-                        if you want to know about me read this
-                    </Typography>
+        <div>
+            <Container maxWidth={'md'} sx={headerContainerStyle}>
+                <Grid container spacing={2}>
+                    <Grid display={'grid'} sx={headerGridStyle} item xs={12} md={7}>
+                        <Typography variant={'h3'} sx={headerTextStyle}>
+                            Hi, I'm Arash
+                        </Typography>
+                        <Typography align={'center'} variant={'caption'} color={'textSecondary'}>
+                            if you want to know about me read this
+                        </Typography>
+                    </Grid>
+                    <Grid display={'flex'} sx={{flexFlow: 'column', alignItems: 'center'}} item xs={12} md={5}>
+                        <img src={me} alt={'me'} style={{width: '300px', height: '300px', borderRadius: '10px'}}/>
+                    </Grid>
                 </Grid>
-                <Grid display={'flex'} sx={{flexFlow: 'column', alignItems: 'center'}} item xs={12} sm={5}>
-                    <img src={img} alt={'sadme'} style={{width: '300px', height: '300px', borderRadius: '10px'}}/>
-                </Grid>
-            </Grid>
+            </Container>
 
-            <Typography variant={'body2'} sx={{marginTop: '1rem'}}>
-                The donkey is a member of the horse family. The words donkey and ass are used to identify the same
-                animal. However, the term donkey is used for domesticated, or tamed, animals. The term ass is more often
-                used when the animal is wild. Donkeys are descendants of the African wild ass. They were first
-                domesticated by humans in Africa. They are now used around the world as working animals.
-            </Typography>
-            <Typography variant={'body2'} sx={{marginTop: '1rem'}}>
-                The average donkey stands 40 inches (102 centimeters) at the shoulder, but different breeds vary greatly
-                in size and shape. Donkeys range from white to gray or black in color. They usually have a dark stripe
-                from the mane to the tail and a crosswise stripe on the shoulders. The mane is short and upright, and
-                the tail has long hair only at the end. The ears are very long and are dark at the base and tip. A
-                donkey can carry heavy loads over rough land. The donkey can handle the uneven ground of the mountains
-                better than a horse can
-            </Typography>.
-            <Typography variant={'body2'} sx={{marginTop: '1rem'}}>
-                On average female donkeys give birth 12 months after mating. Donkeys sometimes breed with horses to
-                produce animals called mules. Donkeys can live up to 50 years.
-            </Typography>
+            <Container maxWidth={'md'}>
+                <Paragraph variant={'body1'} sx={{color: color.secondary, fontWeight: 'bold'}}>
+                    Little About Me
+                </Paragraph>
+                <Paragraph variant={'body1'}>
+                    Hi there, I'm Arash, a computer engineering undergraduate student at <Highlight>Sharif University of
+                    Technology
+                </Highlight>.
+                    My field of interest from web applications to neural networks and computer architecture.
+                </Paragraph>
 
-            <Divider sx={{marginTop:'0.5rem', marginBottom:'0.5rem'}}/>
-        </Container>
+                <Paragraph variant={'body1'}>
+                    Choosing what I want to be is always challenging for me. I remember when I finished high school, I
+                    wasn't sure about what field I liked to study. There were two choices for me 1) study mechanics and
+                    2) study computer engineering. My first choice was driven by my passion for physics, which I
+                    developed in high school, and the second choice was simply because it looked like it needed
+                    creativity or somehow gave that feeling that I could create something new. So I chose to give myself
+                    a challenge and start studying Computer Engineering.
+                </Paragraph>
+
+                <Paragraph variant={'body1'}>
+                    After 3 years of studying, I realized that we can't truly know what we want to be until we try it.
+                    Having a plan for features is good and even necessary, but it's not the only thing that matters.
+                    Having the guts to try new things and sometimes change our plans gives us the flexibility and the
+                    joy of life. Recently I was reading about <Highlight>
+                    Gradient Descent
+                </Highlight> what was funny about it was that it
+
+                    shows when the worlds get complex, finding a maximum or minimum is not easy it all So lets just
+                    learn from it. Take small steps and hope for the best =).
+                </Paragraph>
+
+                <Divider sx={{marginTop: '0.5rem', marginBottom: '0.5rem'}}/>
+            </Container>
+        </div>
     );
 }
