9236c2b0d7466d0e62dd20747fc4b758ff18a92a
Arash <arashyadegari0402@gmail.com>
2022-11-01 20:31:04 +0330

    edit grammar problems

--- src/sections/Introduction.jsx
+++ src/sections/Introduction.jsx
@@ -4,10 +4,6 @@ import color from "../constants/pallate";
 import {StyledDivider, Paragraph, Highlight} from "../components/Components";
 
 
-
-
-
-
 function Introduction() {
     return (
         <Container maxWidth={'md'}>
@@ -23,27 +19,27 @@ function Introduction() {
 
             <Paragraph variant={'body1'}>
                 Choosing what I want to be is always challenging for me. I remember when I finished high school, I
-                wasn't sure about what field I liked to study. There were two choices for me 1) study mechanics and
-                2) study computer engineering. My first choice was driven by my passion for physics, which I
-                developed in high school, and the second choice was simply because it looked like it needed
-                creativity or somehow gave that feeling that I could create something new. So I chose to give myself
-                a challenge and start studying Computer Engineering.
+                wasn't sure about what field I liked to study. There were two choices for me 1) study mechanics and 2)
+                study computer engineering. My first choice was driven by my passion for physics, which I developed in
+                high school, and the second choice was simply because it looked like it needed creativity or somehow
+                gave that feeling that I could create something new. So, I chose to give myself a challenge and start
+                studying Computer Engineering.
             </Paragraph>
 
             <Paragraph variant={'body1'}>
                 After 3 years of studying, I realized that we can't truly know what we want to be until we try it.
-                Having a plan for features is good and even necessary, but it's not the only thing that matters.
-                Having the guts to try new things and sometimes change our plans gives us the flexibility and the
-                joy of life.
+                Having a plan for features is good and even necessary, but it's not the only thing that matters. Having
+                the guts to try new things and sometimes change our plans gives us the flexibility and the joy of life.
             </Paragraph>
 
             <Paragraph variant={'body1'}>
                 Recently I was reading about <Highlight>
                 Gradient Descent
-            </Highlight> what was funny about it was that it
+            </Highlight> what was funny about it was that it shows when the environment gets complex, finding a global
+                max is not
+                easy at all, so let us just learn from this beautiful method and try to take small steps and hope for
+                the best =).
 
-                shows when the worlds get complex, finding a maximum or minimum is not easy it all So lets just
-                learn from it. Take small steps and hope for the best =).
             </Paragraph>
 
             <StyledDivider/>
